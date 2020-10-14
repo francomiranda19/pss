a7a2a71768aa7247c0d0bed032916eb3a69f7e8b
Franco Miranda <francomiranda1901@gmail.com>
2020-10-14 03:08:37 -0300

    Agregados encabezados de funciones

---
+++ Tarea3/t3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "t3.h"
+
+typedef struct nodo {
+    int id, hash;
+    struct nodo *izq, *der;
+} Nodo;
+
+void desbalancear(Nodo **pa, Nodo **pult) {
+
+}
+
+Nodo *desbalanceado(Nodo *a, Nodo *pult) {
+    
+}
