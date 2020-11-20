b5819d50e6cf723b0e000cf517ae5c9818496d56
Franco Miranda <francomiranda1901@gmail.com>
2020-11-20 19:19:01 -0300

    Función creada y resultados

--- Tarea6/t6.c
+++ Tarea6/t6.c
@@ -1,15 +1,23 @@
 #include <pthread.h>
-
 #include "t6.h"
 
-// *** Esta implementacion no funciona porque no respeta orden de llegada ***
-
-pthread_mutex_t m= PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t m1 = PTHREAD_MUTEX_INITIALIZER, m2 = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+int visor;
+int num_entrega;
 
 void ocupar() {
-  pthread_mutex_lock(&m);
+  pthread_mutex_lock(&m1);
+  int mi_num = num_entrega++;
+  pthread_mutex_unlock(&m1);
+  pthread_mutex_lock(&m2);
+  while (visor != mi_num) {
+    pthread_cond_wait(&cond, &m2);
+  }
 }
 
 void desocupar() {
-  pthread_mutex_unlock(&m);
+  visor++;
+  pthread_cond_broadcast(&cond);
+  pthread_mutex_unlock(&m2);
 }
