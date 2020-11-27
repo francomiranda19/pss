72a6c844938fbecd33dd0686e1dcc9198e23b4d6
Franco Miranda <francomiranda1901@gmail.com>
2020-11-27 00:11:16 -0300

    Funciones actualizadas

--- Tarea6/t6.c
+++ Tarea6/t6.c
@@ -1,23 +1,22 @@
 #include <pthread.h>
 #include "t6.h"
 
-pthread_mutex_t m1 = PTHREAD_MUTEX_INITIALIZER, m2 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
-int visor;
-int num_entrega;
+int visor, num_entrega;
 
 void ocupar() {
-  pthread_mutex_lock(&m1);
+  pthread_mutex_lock(&mutex);
   int mi_num = num_entrega++;
-  pthread_mutex_unlock(&m1);
-  pthread_mutex_lock(&m2);
   while (visor != mi_num) {
-    pthread_cond_wait(&cond, &m2);
+    pthread_cond_wait(&cond, &mutex);
   }
+  pthread_mutex_unlock(&mutex);
 }
 
 void desocupar() {
+  pthread_mutex_lock(&mutex);
   visor++;
   pthread_cond_broadcast(&cond);
-  pthread_mutex_unlock(&m2);
+  pthread_mutex_unlock(&mutex);
 }
